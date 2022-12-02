7e5145df174435fb71074fb2992bc15f92500ef2
IvanRdaw <ivanr.daw@gmail.com>
2022-12-02 21:27:00 +0100

    metodo para encontrar bitsnecesarios

--- src/main/java/com/tiernoparla/practica/Ruiz_Del_Portal_Ivan_Practica1.java
+++ src/main/java/com/tiernoparla/practica/Ruiz_Del_Portal_Ivan_Practica1.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class Ruiz_Del_Portal_Ivan_Practica1 {
-    
+
     // Se crea el Mensaje random en Array
     static int[] MensajeRandom(int longitud) {
         int nrandom;
@@ -16,20 +16,28 @@ public class Ruiz_Del_Portal_Ivan_Practica1 {
         }//for
         return (mensaje);
     }//MensajeRandom
-    
+
+    static int[] Bitsnecesarios(int[] mensaje) {
+        int nbits;
+        double p = 1;
+        while (Math.pow(2, p) < p + mensaje.length + 1) {
+            p++;
+        } //for
+        nbits = (int) p;
+        //System.out.println(nbits);
+        int[] bitsnecesarios = new int[nbits + mensaje.length + 1];
+        return bitsnecesarios;
+    } //sender
+
     public static void main(String[] args) {
         int[] mensaje;
-        
+        int[] bitsnecesarios;
+
         mensaje = MensajeRandom(10);
-        System.out.println(Arrays.toString(mensaje)); 
+        System.out.println(Arrays.toString(mensaje));
+
+        bitsnecesarios = Bitsnecesarios(mensaje);
+        System.out.println(Arrays.toString(bitsnecesarios));
     } //main
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 } //Practica1

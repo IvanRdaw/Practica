e79df30adc1d36ca83739900faaf6286245d8577
IvanRdaw <ivanr.daw@gmail.com>
2022-12-02 21:29:44 +0100

    metodo para añadir mensaje y bits redudancia a array

--- src/main/java/com/tiernoparla/practica/Ruiz_Del_Portal_Ivan_Practica1.java
+++ src/main/java/com/tiernoparla/practica/Ruiz_Del_Portal_Ivan_Practica1.java
@@ -17,6 +17,7 @@ public class Ruiz_Del_Portal_Ivan_Practica1 {
         return (mensaje);
     }//MensajeRandom
 
+    //Busca la cantidad de bits necesarios para el mensaje
     static int[] Bitsnecesarios(int[] mensaje) {
         int nbits;
         double p = 1;
@@ -29,15 +30,36 @@ public class Ruiz_Del_Portal_Ivan_Practica1 {
         return bitsnecesarios;
     } //sender
 
+    //añade el mensaje a la Array y pone 2 en cada bit de redundancia
+    static int[] Sendermedio(int[] mensaje) {
+        int posibit = 0;
+        int posimens = 0;
+        int[] sender = Bitsnecesarios(mensaje);
+        for (int i = 1; i < sender.length; i++) {
+            if (i == Math.pow(2, posibit)) {
+                sender[i] = 2;
+                posibit++;
+            } //if
+            else {
+                sender[i] = mensaje[posimens++];
+            } //else
+        } //for
+        return sender;
+    }//sendermedio
+    
     public static void main(String[] args) {
         int[] mensaje;
         int[] bitsnecesarios;
-
+        int[] sender;
+        
         mensaje = MensajeRandom(10);
         System.out.println(Arrays.toString(mensaje));
 
         bitsnecesarios = Bitsnecesarios(mensaje);
         System.out.println(Arrays.toString(bitsnecesarios));
+        
+        sender = Sendermedio(mensaje);
+        System.out.println(Arrays.toString(sender));
     } //main
 
 } //Practica1
